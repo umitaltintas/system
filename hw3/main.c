bc7103455eba2ada55994e109a3fe4fc069d356c
umitaltintas <umitaltintas1@gmail.com>
2021-04-18 06:55:30 +0300

    save hw3

--- hw3/main.c
+++ hw3/main.c
@@ -1,38 +1,42 @@
 //
 // Created by umit on 17.04.2021.
 //
-#include <stdlib.h>
-#include <stdio.h>
-#include <getopt.h>
-#include <string.h>
 #include "usage.h"
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char **argv) {
 
-    int opt;
-
-    if (argc < 2) {
-        printUsage();
-        exit(EXIT_FAILURE);
+  int c;
+  char *shared;
+  char *fifo;
+  int pat;
+  if (argc < 7) {
+    printUsage();
+    exit(EXIT_FAILURE);
+  }
+  while ((c = getopt(argc, argv, "b:s:f:")) != -1) {
+    switch (c) {
+    case 'f':
+      printf("f: %s\n", optarg);
+      fifo = optarg;
+      break;
+    case 'b':
+      printf("b: %s\n", optarg);
+      pat = atoi(optarg);
+      break;
+    case 's':
+      printf("s: %s\n", optarg);
+      shared = optarg;
+      break;
+    default:
+      printUsage();
+      exit(EXIT_FAILURE);
     }
-    int c;
-    while ((c = getopt(argc, argv, "b:s:f:")) != -1) {
-        switch (c) {
-            case 'f':
-                printf("f: %s\n", optarg);
-                break;
-            case 'b':
-                printf("b: %s\n", optarg);
-                break;
-            case 's':
-                printf("s: %s\n", optarg);
-                break;
+  }
+  printf("shared: %s , fifo: %s, pat: %d", shared, fifo, pat);
 
-            default:
-                printUsage();
-                exit(EXIT_FAILURE);
-        }
-
-
-    }
+  return 0;
 }
